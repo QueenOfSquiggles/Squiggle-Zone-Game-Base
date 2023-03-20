23a79fcf53e1438017dba069f6340bbe8d0c1750
QueenOfSquiggles <8940604+QueenOfSquiggles@users.noreply.github.com>
2023-03-19 20:32:51 -0500

    Brief backup before updating to 4.0.1 RC 2

--- Scenes/ShaderSystems/set_shader_global_position.gd
+++ Scenes/ShaderSystems/set_shader_global_position.gd
@@ -2,5 +2,5 @@ extends Marker3D
 
 @export var uniform_name := ""
 
-func _process(delta):
+func _process(_delta):
 	RenderingServer.global_shader_parameter_set(uniform_name, global_position)
--- Scenes/World/world_base.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var hud_req := $"HUD Requests"
-
-func _ready() -> void:
-	print("World Base is now ready")
-	hud_req.call("RequestSubtitle", "This is a subtitle")
-
